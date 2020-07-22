26a1ce8ac818b38938c3b876b2a1686434786750
dooyou <strawberry.daeyun@gmail.com>
2020-07-23 00:09:37 +0900

    2일차 JSX

--- src/App.js
+++ src/App.js
@@ -3,24 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const element = '강아지';
+  const element2 = '고양이';
+  const num = 0;
+  const name = undefined;
+  const style = {
+    backgroundColor : '#dddddd',
+    padding : 20,
+    fontWeight : 'bold'
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {element === '강아지' ? (<h1>강아지 입니다.</h1>) : null}
+      {element2 === '강아지' && <h1>강아지 입니다.</h1>}
+      {num && <h1>숫자</h1>}
+      {name || ' 값이 undefined입니다.'}
+      <h1 style={style}>{element}</h1>
+    </>
   );
 }
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
 
 export default App;
