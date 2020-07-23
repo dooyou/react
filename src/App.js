98d68d061ed2317a35ddd6b790ac4814265e2f4e
dooyou <strawberry.daeyun@gmail.com>
2020-07-24 01:03:31 +0900

    컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MyComponent from './MyComponent';
 
-function App() {
-  const element = '강아지';
-  const element2 = '고양이';
-  const num = 0;
-  const name = undefined;
-  const style = {
-    backgroundColor : '#dddddd',
-    padding : 20,
-    fontWeight : 'bold'
-  };
-  return (
-    <>
-      {element === '강아지' ? (<h1>강아지 입니다.</h1>) : null}
-      {element2 === '강아지' && <h1>강아지 입니다.</h1>}
-      {num && <h1>숫자</h1>}
-      {name || ' 값이 undefined입니다.'}
-      <h1 style={style}>{element}</h1>
-    </>
-  );
-}
+const App = () => {
+  return <MyComponent name="강아지" favoriteNumber={3}>고양이</MyComponent>;
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import logo from './logo.svg';
+//import './App.css';
+
+// function App() {
+//   const element = '강아지';
+//   const element2 = '고양이';
+//   const num = 0;
+//   const name = undefined;
+//   const style = {
+//     backgroundColor : '#dddddd',
+//     padding : 20,
+//     fontWeight : 'bold'
+//   };
+//   return (
+//     <>
+//       {element === '강아지' ? (<h1>강아지 입니다.</h1>) : null}
+//       {element2 === '강아지' && <h1>강아지 입니다.</h1>}
+//       {num && <h1>숫자</h1>}
+//       {name || ' 값이 undefined입니다.'}
+//       <h1 style={style}>{element}</h1>
+//     </>
+//   );
+// }
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
@@ -38,6 +59,3 @@ function App() {
     //     </a>
     //   </header>
     // </div>
-
-
-export default App;
