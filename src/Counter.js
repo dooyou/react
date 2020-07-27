16dfd563f75e82224039ce516d4df5c1051209d3
dooyou <strawberry.daeyun@gmail.com>
2020-07-27 23:55:22 +0900

    state

---
+++ src/Counter.js
@@ -0,0 +1,23 @@
+import React, { Component }  from 'react';
+import PropTypes from 'prop-types';
+
+class Counter extends Component {
+  constructor(props){
+    super(grops);
+    this.state = {
+      number:0
+    };
+  }
+
+  render() {
+    const { number } = this.state; // sate를 조회 할 때는 this.state로 조회합니다
+    <div>
+      <h1>{number}</h1>
+      <button
+         onClick = { 0 => {
+           this.setState({ number : number + 1});
+     );
+    }
+  }
+
+  export default MyComponent;
