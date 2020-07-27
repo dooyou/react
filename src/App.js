d61ce8dc60b442dc9310be73898ae6369b2b3a34
dooyou <strawberry.daeyun@gmail.com>
2020-07-28 01:28:32 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,62 +1,12 @@
-import React from 'react';
-import MyComponent from './MyComponent';
+import React, { Component } from 'react';
+import Welcome from './MyComponent3';
 
 
-const App = () => {
-  return <MyComponent name="강아지" favoriteNumber={3}>고양이</MyComponent>;
-};
+class App extends Component {
+  render(){
+    const name = 'Sara';
+    return <div className='react'>{name}</div>;
+  }
+}
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//import logo from './logo.svg';
-//import './App.css';
-
-// function App() {
-//   const element = '강아지';
-//   const element2 = '고양이';
-//   const num = 0;
-//   const name = undefined;
-//   const style = {
-//     backgroundColor : '#dddddd',
-//     padding : 20,
-//     fontWeight : 'bold'
-//   };
-//   return (
-//     <>
-//       {element === '강아지' ? (<h1>강아지 입니다.</h1>) : null}
-//       {element2 === '강아지' && <h1>강아지 입니다.</h1>}
-//       {num && <h1>숫자</h1>}
-//       {name || ' 값이 undefined입니다.'}
-//       <h1 style={style}>{element}</h1>
-//     </>
-//   );
-// }
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
---
+++ src/MyComponent3.js
@@ -0,0 +1,10 @@
+import React, { Component }  from 'react';
+import PropTypes from 'prop-types';
+
+function Welcome(props){
+  return <h1>Hello, {props.name}</h1>;
+}
+
+
+
+export default Welcome;
